43518ea292baefa0d2c81c1de5b55df7a1120394
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-09 17:18:56 +0300

    Update find&replace.py

--- find&replace.py
+++ find&replace.py
@@ -10,7 +10,9 @@ def findErrorNums(nums):
         for x in range(n):
             if nums.count(nums[x])>1:
                 a=nums[x]
+        #remove the number from the array
         nums.pop(nums.index(a))
+        #check through the order and keep count of the missing number and find it
         for y in range(len(nums)):
             if nums[y]!=y+1 :
                 b=y+1
