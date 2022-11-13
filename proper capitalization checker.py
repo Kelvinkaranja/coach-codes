829188d4c19b097f10f3cd2904b291168aa7591b
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-13 12:15:21 +0300

    Update proper capitalization checker.py

--- proper capitalization checker.py
+++ proper capitalization checker.py
@@ -3,10 +3,13 @@ def detectCapitalUse(word):
         :type word: str
         :rtype: bool
     """
+    #Find all caps
     if word.isupper():
         return True
+    #Find all lower
     elif word.islower():
         return True
+    #Check if first digit is upper and the rest lower
     elif word[1:].islower():
         return True
     else:
