90a6a9eb59eb886bf0a884be68e8eaef0e7ebc6a
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-13 12:12:51 +0300

    Update third larget number.py

--- third larget number.py
+++ third larget number.py
@@ -9,5 +9,6 @@ def thirdMax(nums):
         #Find the third last digit in the list if it exists
         if len(s)>2:
             return s[-3]
+        #Find the largest number if the third larget
         else:
             return max(s)
