913cf64eac0869cf5f733cd49895a85edbd144cc
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-13 11:33:37 +0300

    Update third larget number.py

--- third larget number.py
+++ third larget number.py
@@ -3,8 +3,10 @@ def thirdMax(nums):
         :type nums: List[int]
         :rtype: int
         """
+        #Create unique list of nums and sort it
         s=list(set(nums))
         s.sort()
+        #Find the third last digit in the list if it exists
         if len(s)>2:
             return s[-3]
         else:
