6edff61374185bed783e6e5a4697c5bba1da6b51
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-14 12:38:44 +0300

    Update string map.py

--- string map.py
+++ string map.py
@@ -1,6 +1,9 @@
 def isIsomorphic(s: str, t: str) -> bool:
+    #Declare variables
     d, n = {}, len(s)
+    #iterate through string
     for i in range(n):
+        #Isolate edge case
         if s[i] in d and d[s[i]] != t[i]:
             return False
         d[s[i]] = t[i]
