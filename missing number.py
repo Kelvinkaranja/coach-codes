9b0d90a97ad4ab1e885b7ff457f4e97ebcd64fde
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-18 08:38:35 +0300

    Update missing number.py

--- missing number.py
+++ missing number.py
@@ -3,7 +3,7 @@ def missingNumber(nums):
         :type nums: List[int]
         :rtype: int
         """
-        #Declare Variables
+        #Declare Variables and sort your list
         n=len(nums)
         nums.sort()
         for e in range(n+1):
