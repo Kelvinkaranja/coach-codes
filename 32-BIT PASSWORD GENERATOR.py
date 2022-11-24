df75012c8fae2c58d2467b5ce737fb05d355efde
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-11-24 22:21:06 +0300

    Update 32-BIT PASSWORD GENERATOR.py

--- 32-BIT PASSWORD GENERATOR.py
+++ 32-BIT PASSWORD GENERATOR.py
@@ -51,7 +51,7 @@ def password_generator(length=4):
         #add the character to the password
         user_password += layer_two
 
-        #continue loop until password is fully generated
+        #continue loop until password is fully generated.
         password_length -= 1
 
     return user_password
