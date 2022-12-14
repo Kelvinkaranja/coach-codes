2bff48eb54c1f1fa0cf70ce76b1b3e19bec1017d
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-12-14 23:49:51 +0300

    Add files via upload

--- 2sum.py
+++ 2sum.py
@@ -1,17 +1,18 @@
+'''
+Find 2 numbers in an array that ad up to a certain given target.
+'''
+
 def twoSum(nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        #Declare variables
         n=len(nums)
         k=0
-        #Loop through to find difference
         while n>0:
             diff=target-nums[k]
-        #Isolate difference IF FOUND
-            if diff in nums:
+            if diff in nums and nums.index(diff) != k:
                 return [k,nums.index(diff)]
             else:
                 k+=1
