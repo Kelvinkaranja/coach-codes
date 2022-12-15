69a3cc27bacc9180856a8221167c8d996c3fa389
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-12-15 08:20:45 +0300

    Update greatestrepeatedletter.py

--- greatestrepeatedletter.py
+++ greatestrepeatedletter.py
@@ -1,4 +1,10 @@
 def greatestLetter(self, s):
+        '''
+        Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. 
+        The returned letter should be in uppercase. If no such letter exists, return an empty string.
+
+        An English letter b is greater than another letter a if b appears after a in the English alphabet.
+        '''
         """
         :type s: str
         :rtype: str
