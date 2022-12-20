68ac432c10ab97e1636111bd7572669088d57142
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-12-20 15:08:38 +0300

    Update ticketing.py

--- ticketing.py
+++ ticketing.py
@@ -10,6 +10,9 @@ def timeRequiredToBuy(self, tickets, k):
             for i in range(len(tickets)):
                 if tickets[k]==0:
                     return time
-                
+                if tickets[i]>0:
+                    tickets[i]=tickets[i]-1
+                    time +=1
+        
         
         
